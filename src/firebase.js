6ae2ec8d3fae1678d8c589cdf8b11bce7c8b9ff7
s1xte3n <ntnhlapo16@gmail.com>
2025-03-14 02:15:01 +0200

    Homepage Mafe

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBGGKphF9o9N-0KAJxAVGVNucNMWhPHJ5g",
-  authDomain: "webcafelanding-20f9d.firebaseapp.com",
-  projectId: "webcafelanding-20f9d",
-  storageBucket: "webcafelanding-20f9d.firebasestorage.app",
-  messagingSenderId: "1036214201993",
-  appId: "1:1036214201993:web:7a7f5cb0f05f86c952e058",
-  measurementId: "G-1PVGCT5C8P"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
