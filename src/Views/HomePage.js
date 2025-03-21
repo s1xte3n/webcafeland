ea0d2467cb91bf63195361658dabab280c2dbef4
s1xte3n <ntnhlapo16@gmail.com>
2025-03-22 00:32:44 +0200

    Solid TopFold

--- src/Views/HomePage.js
+++ src/Views/HomePage.js
@@ -5,12 +5,6 @@ import './HomePage.css';
 import TopFold from "./TopFold.js";
 
 const HomePage = () => {
-    const [isBlue, setIsBlue] = useState(false);
-    
-    const toggleColor = () => {
-        setIsBlue(!isBlue);
-    };
-
     return (
         <div className="cafe-homepage">
             <TopFold />
