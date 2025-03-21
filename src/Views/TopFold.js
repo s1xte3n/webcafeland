061dca768e2a3346979fd4a246c21833e3c8d9bb
s1xte3n <ntnhlapo16@gmail.com>
2025-03-21 04:51:27 +0200

    TopFold New

--- src/Views/TopFold.js
+++ src/Views/TopFold.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './TopFold.css';
 import webcafeBeer from '../assets/images/webcafe-beer-v1.png';
-import webcafeLogo from '../assets/images/logo512.png';
 
 const TopFold = () => {
   return (
