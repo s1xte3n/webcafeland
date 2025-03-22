e037d824417864bf3d9c012ca3d1e72f3b1a2ec4
s1xte3n <ntnhlapo16@gmail.com>
2025-03-22 23:33:54 +0200

    Investment Done

--- src/Views/HomePage.js
+++ src/Views/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner"; // Adjust path accordingly
 import TopFold from "./TopFold.js";
+import Investment from "./Investment.js";
 
 const HomePage = () => {
     return (
         <div>
             <Banner newsBulletin="Latest news: WebCafe's new AI features are live!" />
             <TopFold />
+            <Investment />
         </div>
     );
 };
